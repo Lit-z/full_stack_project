5abf2786d02bff88caee4efb9b83e78444671910
Jamie <gee.jamie.a@gmail.com>
2022-05-19 12:47:51 +0100

    first heroku commit

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ app.register_blueprint(my_view)
 def page_not_found(e):
     return render_template('404.html',e=e)
 
+@app.errorhandler(500)
+def internal_error(e):
+    return render_template('500.html',e=e)
+
 # to run the site with debug settings than the default via running app.py
 if __name__ == '__main__':
     app.run(debug=True,port=8000)
--- views.py
+++ views.py
@@ -12,28 +12,30 @@ def home():
 def admin():
     return render_template('admin.html')
 
-@my_view.route('/page1')
+@my_view.route('/Detection')
 def page1():
     return render_template('page1.html')
 
-@my_view.route('/page2')
+@my_view.route('/Types')
 def page2():
     return render_template('page2.html')
 
-@my_view.route('/page3')
+@my_view.route('/Stars')
 def page3():
     return render_template('page3.html')
 
-@my_view.route('/page4')
+@my_view.route('/Data')
 def page4():
     return render_template('page4.html')
 
-@my_view.route('/page5')
+@my_view.route('/Gallery')
 def page5():
     return render_template('page5.html')
 
+# redirects of url's that will send user to the homepage
 @my_view.route('/js')
 @my_view.route('/javascript')
 @my_view.route('/home')
+@my_view.route('/index')
 def js_redirect():
     return redirect(url_for('my_view.home'))
